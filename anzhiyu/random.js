4280c31baddf93a64457477cbafdbfa430f2af51
abinng <abinng@outlook.com>
2026-01-21 11:02:34 +0800

    Site updated: 2026-01-21 11:02:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/37411.html","posts/16107.html","posts/8878.html","posts/56777.html"];function toRandomPost(){
+var posts=["posts/56777.html","posts/16107.html","posts/8878.html","posts/37411.html","posts/44655.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
