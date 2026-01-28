9b89847879bf04bb0d5e43f5945a5235a02c1080
abinng <abinng@outlook.com>
2026-01-28 23:50:20 +0800

    Site updated: 2026-01-28 23:50:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/16107.html/","posts/56777.html/","posts/7125.html/","posts/8878.html/","posts/37411.html/","posts/59966.html/","posts/41031.html/","posts/44655.html/","posts/6882.html/"];function toRandomPost(){
+var posts=["posts/56777.html/","posts/16107.html/","posts/8878.html/","posts/59966.html/","posts/7125.html/","posts/41031.html/","posts/37411.html/","posts/59522.html/","posts/6882.html/","posts/44655.html/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
